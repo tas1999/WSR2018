18c5541717a0cbd9ee691d22a760b8101bd3c7f3
stanislav aksentov <rekym@yandex.ru>
2018-10-29 09:34:09 +0300

    First Commit

---
+++ WSR2018/MarafonTimer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace WSR2018
+{
+    public class MarafonTimer
+    {
+        DateTime MarafonTime = new DateTime(2018, 11, 24, 6, 0, 0);
+        TextBlock TimerTBox { get; set; }
+        public MarafonTimer(TextBlock timerTBox)
+        {
+            TimerTBox = timerTBox;
+        }
+        public void Start()
+        {
+            var timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 1, 0);
+            timer.Tick += (o, arg) =>
+            {
+                var time = MarafonTime - DateTime.Now;
+                TimerTBox.Text = $"{time.Days} дней {time.Hours} часов и {time.Minutes} минут {time.Seconds} секунд  до старта марафона";
+            };
+            timer.Start();
+        }
+
+    }
+}
---
+++ WSR2018/SponsorARunnerWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WSR2018
+{
+    /// <summary>
+    /// Логика взаимодействия для SponsorARunnerWindow.xaml
+    /// </summary>
+    public partial class SponsorARunnerWindow : Window
+    {
+        public SponsorARunnerWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddMoney_Click(object sender, RoutedEventArgs e)
+        {
+            double money = double.Parse(MoneyTBox.Text);
+            money += 10;
+            MoneyTBox.Text = money.ToString();
+            MoneyTBlock.Text = "$" + money.ToString();
+        }
+
+        private void RemoveMoney_Click(object sender, RoutedEventArgs e)
+        {
+            double money = double.Parse(MoneyTBox.Text);
+            money -= 10;
+            MoneyTBox.Text = money.ToString();
+            MoneyTBlock.Text = "$" + money.ToString();
+        }
+
+        private void MoneyTBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(MoneyTBox.Text)) MoneyTBox.Text = "0";
+           MoneyTBlock.Text = "$" + MoneyTBox.Text;
+        }
+
+        
+        private void MoneyTBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!Char.IsDigit(e.Text, 0)) e.Handled = true;
+        }
+    }
+}
